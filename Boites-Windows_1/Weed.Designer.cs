5bdc6f39c92462e2b3108c175e3d15e3a870d636
Chris Trombly <christrombly001@hotmail.com>
2026-01-01 14:05:41 -0500

    Weed Module v1 Designed

---
+++ Boites-Windows_1/Weed.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace Boites_Windows_1
+{
+    partial class Weed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Weed));
+            weedHeader = new Label();
+            dateLabel = new Label();
+            startText = new TextBox();
+            finishLabel = new Label();
+            startLabel = new Label();
+            dateText = new TextBox();
+            finishText = new TextBox();
+            durationLabel = new Label();
+            durationText = new TextBox();
+            brandText = new TextBox();
+            brandLabel = new Label();
+            finish = new Button();
+            start = new Button();
+            moduleLogo = new PictureBox();
+            indicaButton = new RadioButton();
+            sativaButton = new RadioButton();
+            hybridButton = new RadioButton();
+            classification = new GroupBox();
+            amountLabel = new Label();
+            textBox1 = new TextBox();
+            jointButton = new RadioButton();
+            pipeButton = new RadioButton();
+            bongButton = new RadioButton();
+            vaporizerButton = new RadioButton();
+            methodLabel = new Label();
+            methodBox = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)moduleLogo).BeginInit();
+            classification.SuspendLayout();
+            methodBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // weedHeader
+            // 
+            weedHeader.AutoSize = true;
+            weedHeader.Font = new Font("Retro Gaming", 27F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            weedHeader.Location = new Point(12, 9);
+            weedHeader.Name = "weedHeader";
+            weedHeader.Size = new Size(135, 46);
+            weedHeader.TabIndex = 70;
+            weedHeader.Text = "Weed";
+            // 
+            // dateLabel
+            // 
+            dateLabel.AutoSize = true;
+            dateLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dateLabel.Location = new Point(12, 231);
+            dateLabel.Name = "dateLabel";
+            dateLabel.Size = new Size(57, 29);
+            dateLabel.TabIndex = 73;
+            dateLabel.Text = "Date:";
+            // 
+            // startText
+            // 
+            startText.Location = new Point(73, 156);
+            startText.Name = "startText";
+            startText.Size = new Size(130, 23);
+            startText.TabIndex = 74;
+            // 
+            // finishLabel
+            // 
+            finishLabel.AutoSize = true;
+            finishLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            finishLabel.Location = new Point(3, 189);
+            finishLabel.Name = "finishLabel";
+            finishLabel.Size = new Size(66, 29);
+            finishLabel.TabIndex = 72;
+            finishLabel.Text = "Finish:";
+            // 
+            // startLabel
+            // 
+            startLabel.AutoSize = true;
+            startLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            startLabel.Location = new Point(7, 150);
+            startLabel.Name = "startLabel";
+            startLabel.Size = new Size(62, 29);
+            startLabel.TabIndex = 71;
+            startLabel.Text = "Start:";
+            // 
+            // dateText
+            // 
+            dateText.Location = new Point(73, 237);
+            dateText.Name = "dateText";
+            dateText.Size = new Size(130, 23);
+            dateText.TabIndex = 76;
+            // 
+            // finishText
+            // 
+            finishText.Location = new Point(73, 195);
+            finishText.Name = "finishText";
+            finishText.Size = new Size(130, 23);
+            finishText.TabIndex = 75;
+            // 
+            // durationLabel
+            // 
+            durationLabel.AutoSize = true;
+            durationLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            durationLabel.Location = new Point(7, 273);
+            durationLabel.Name = "durationLabel";
+            durationLabel.Size = new Size(90, 29);
+            durationLabel.TabIndex = 77;
+            durationLabel.Text = "Duration:";
+            // 
+            // durationText
+            // 
+            durationText.Location = new Point(105, 279);
+            durationText.Name = "durationText";
+            durationText.Size = new Size(98, 23);
+            durationText.TabIndex = 78;
+            // 
+            // brandText
+            // 
+            brandText.Location = new Point(311, 156);
+            brandText.Name = "brandText";
+            brandText.Size = new Size(111, 23);
+            brandText.TabIndex = 71;
+            // 
+            // brandLabel
+            // 
+            brandLabel.AutoSize = true;
+            brandLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            brandLabel.Location = new Point(231, 150);
+            brandLabel.Name = "brandLabel";
+            brandLabel.Size = new Size(68, 29);
+            brandLabel.TabIndex = 70;
+            brandLabel.Text = "Brand:";
+            // 
+            // finish
+            // 
+            finish.BackColor = Color.White;
+            finish.Font = new Font("Retro Gaming", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            finish.ForeColor = Color.DarkGreen;
+            finish.Location = new Point(231, 375);
+            finish.Name = "finish";
+            finish.Size = new Size(191, 64);
+            finish.TabIndex = 79;
+            finish.Text = "Finish";
+            finish.UseVisualStyleBackColor = false;
+            // 
+            // start
+            // 
+            start.BackColor = Color.DarkGreen;
+            start.Font = new Font("Retro Gaming", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            start.ForeColor = SystemColors.ButtonFace;
+            start.Location = new Point(12, 375);
+            start.Name = "start";
+            start.Size = new Size(191, 64);
+            start.TabIndex = 80;
+            start.Text = "Start";
+            start.UseVisualStyleBackColor = false;
+            // 
+            // moduleLogo
+            // 
+            moduleLogo.Image = (Image)resources.GetObject("moduleLogo.Image");
+            moduleLogo.InitialImage = null;
+            moduleLogo.Location = new Point(339, 12);
+            moduleLogo.Name = "moduleLogo";
+            moduleLogo.Size = new Size(83, 86);
+            moduleLogo.TabIndex = 81;
+            moduleLogo.TabStop = false;
+            // 
+            // indicaButton
+            // 
+            indicaButton.AutoSize = true;
+            indicaButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            indicaButton.Location = new Point(20, 30);
+            indicaButton.Name = "indicaButton";
+            indicaButton.Size = new Size(73, 19);
+            indicaButton.TabIndex = 82;
+            indicaButton.TabStop = true;
+            indicaButton.Text = "Indica";
+            indicaButton.UseVisualStyleBackColor = true;
+            // 
+            // sativaButton
+            // 
+            sativaButton.AutoSize = true;
+            sativaButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            sativaButton.Location = new Point(99, 30);
+            sativaButton.Name = "sativaButton";
+            sativaButton.Size = new Size(72, 19);
+            sativaButton.TabIndex = 83;
+            sativaButton.TabStop = true;
+            sativaButton.Text = "Sativa";
+            sativaButton.UseVisualStyleBackColor = true;
+            // 
+            // hybridButton
+            // 
+            hybridButton.AutoSize = true;
+            hybridButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            hybridButton.Location = new Point(55, 55);
+            hybridButton.Name = "hybridButton";
+            hybridButton.Size = new Size(73, 19);
+            hybridButton.TabIndex = 84;
+            hybridButton.TabStop = true;
+            hybridButton.Text = "Hybrid";
+            hybridButton.UseVisualStyleBackColor = true;
+            // 
+            // classification
+            // 
+            classification.Controls.Add(hybridButton);
+            classification.Controls.Add(sativaButton);
+            classification.Controls.Add(indicaButton);
+            classification.Font = new Font("Orange Kid", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            classification.Location = new Point(229, 186);
+            classification.Name = "classification";
+            classification.Size = new Size(196, 85);
+            classification.TabIndex = 85;
+            classification.TabStop = false;
+            classification.Text = "Strain Classification";
+            // 
+            // amountLabel
+            // 
+            amountLabel.AutoSize = true;
+            amountLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            amountLabel.Location = new Point(229, 273);
+            amountLabel.Name = "amountLabel";
+            amountLabel.Size = new Size(82, 29);
+            amountLabel.TabIndex = 86;
+            amountLabel.Text = "Amount:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(311, 279);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(111, 23);
+            textBox1.TabIndex = 87;
+            // 
+            // jointButton
+            // 
+            jointButton.AutoSize = true;
+            jointButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            jointButton.Location = new Point(14, 14);
+            jointButton.Name = "jointButton";
+            jointButton.Size = new Size(63, 19);
+            jointButton.TabIndex = 88;
+            jointButton.TabStop = true;
+            jointButton.Text = "Joint";
+            jointButton.UseVisualStyleBackColor = true;
+            // 
+            // pipeButton
+            // 
+            pipeButton.AutoSize = true;
+            pipeButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            pipeButton.Location = new Point(83, 14);
+            pipeButton.Name = "pipeButton";
+            pipeButton.Size = new Size(56, 19);
+            pipeButton.TabIndex = 89;
+            pipeButton.TabStop = true;
+            pipeButton.Text = "Pipe";
+            pipeButton.UseVisualStyleBackColor = true;
+            // 
+            // bongButton
+            // 
+            bongButton.AutoSize = true;
+            bongButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            bongButton.Location = new Point(147, 14);
+            bongButton.Name = "bongButton";
+            bongButton.Size = new Size(61, 19);
+            bongButton.TabIndex = 90;
+            bongButton.TabStop = true;
+            bongButton.Text = "Bong";
+            bongButton.UseVisualStyleBackColor = true;
+            // 
+            // vaporizerButton
+            // 
+            vaporizerButton.AutoSize = true;
+            vaporizerButton.Font = new Font("Retro Gaming", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            vaporizerButton.Location = new Point(220, 14);
+            vaporizerButton.Name = "vaporizerButton";
+            vaporizerButton.Size = new Size(99, 19);
+            vaporizerButton.TabIndex = 91;
+            vaporizerButton.TabStop = true;
+            vaporizerButton.Text = "Vaporizer";
+            vaporizerButton.UseVisualStyleBackColor = true;
+            // 
+            // methodLabel
+            // 
+            methodLabel.AutoSize = true;
+            methodLabel.Font = new Font("Orange Kid", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            methodLabel.Location = new Point(7, 325);
+            methodLabel.Name = "methodLabel";
+            methodLabel.Size = new Size(79, 29);
+            methodLabel.TabIndex = 92;
+            methodLabel.Text = "Method:";
+            // 
+            // methodBox
+            // 
+            methodBox.Controls.Add(vaporizerButton);
+            methodBox.Controls.Add(bongButton);
+            methodBox.Controls.Add(pipeButton);
+            methodBox.Controls.Add(jointButton);
+            methodBox.Location = new Point(91, 321);
+            methodBox.Name = "methodBox";
+            methodBox.Size = new Size(329, 45);
+            methodBox.TabIndex = 93;
+            methodBox.TabStop = false;
+            // 
+            // Weed
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(434, 451);
+            Controls.Add(methodBox);
+            Controls.Add(methodLabel);
+            Controls.Add(textBox1);
+            Controls.Add(amountLabel);
+            Controls.Add(classification);
+            Controls.Add(moduleLogo);
+            Controls.Add(finish);
+            Controls.Add(start);
+            Controls.Add(brandText);
+            Controls.Add(brandLabel);
+            Controls.Add(durationText);
+            Controls.Add(weedHeader);
+            Controls.Add(dateLabel);
+            Controls.Add(durationLabel);
+            Controls.Add(startText);
+            Controls.Add(finishText);
+            Controls.Add(finishLabel);
+            Controls.Add(dateText);
+            Controls.Add(startLabel);
+            Name = "Weed";
+            Text = "Weed";
+            ((System.ComponentModel.ISupportInitialize)moduleLogo).EndInit();
+            classification.ResumeLayout(false);
+            classification.PerformLayout();
+            methodBox.ResumeLayout(false);
+            methodBox.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label weedHeader;
+        private Label dateLabel;
+        private TextBox startText;
+        private Label finishLabel;
+        private Label startLabel;
+        private TextBox dateText;
+        private TextBox finishText;
+        private Label durationLabel;
+        private TextBox durationText;
+        private TextBox brandText;
+        private Label brandLabel;
+        private Button finish;
+        private Button start;
+        private PictureBox moduleLogo;
+        private RadioButton indicaButton;
+        private RadioButton sativaButton;
+        private RadioButton hybridButton;
+        private GroupBox classification;
+        private Label amountLabel;
+        private TextBox textBox1;
+        private RadioButton jointButton;
+        private RadioButton pipeButton;
+        private RadioButton bongButton;
+        private RadioButton vaporizerButton;
+        private Label methodLabel;
+        private GroupBox methodBox;
+    }
+}
